d8584ac5d4fdc341361c2214d9769f904cf2d5e9
Lucas Lafèré <laferelucas@gmail.com>
2022-10-03 15:13:00 -0300

    feat: added items pricing, added items repository functions

--- src/repositories/itemsRepository.ts
+++ src/repositories/itemsRepository.ts
@@ -3,7 +3,20 @@ import { ItemsData } from '../types/itemsType'
 
 export async function insert (
     portfolioId: number,
-    
+    title: string,
+    imageUrl: string,
+    description?: string,
+    price?: number
 ) {
-
+    const result = await prisma.items.create({
+        data: {portfolioId, title, imageUrl, description, price},
+        
+    });
+    return result;
+}
+export async function findAll (portfolioId: number){
+    const result = await prisma.items.findMany({
+        where: {portfolioId}
+    })
+    return result;
 }
