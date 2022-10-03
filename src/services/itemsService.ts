13da9cbe3f0bdac03dd8bac5ae3101d2000faddd
Lucas Lafèré <laferelucas@gmail.com>
2022-10-03 15:16:56 -0300

    feat: added findAllItems, deleteItem services

--- src/services/itemsService.ts
+++ src/services/itemsService.ts
@@ -4,3 +4,12 @@ export async function insert (portfolioId: number, title: string, imageUrl: stri
     const result = await itemsRepository.insert(portfolioId, title, imageUrl, description, price);
     return result;
 }
+export async function findAllItemsFromPortfolio (portfolioId:number){
+    const result = await itemsRepository.findAll(portfolioId);
+    return result
+}
+
+export async function deleteItem (id: number){
+    const result = await itemsRepository.deleteItem(id);
+    return result
+}
