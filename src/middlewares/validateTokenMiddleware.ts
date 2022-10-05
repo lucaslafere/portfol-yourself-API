79f9c685f254f02e51902d673f8154ff13a9df82
Lucas Lafèré <laferelucas@gmail.com>
2022-10-05 17:52:00 -0300

    fix: fixed no token status code

--- src/middlewares/validateTokenMiddleware.ts
+++ src/middlewares/validateTokenMiddleware.ts
@@ -4,7 +4,7 @@ import * as manipulateToken from '../utils/manipulateToken';
 
 export async function validateToken(req: Request, res: Response, next: NextFunction) {
     const token = req.headers.authorization?.replace("Bearer ", "");
-    if (!token) throw {type: 'Generic', message: "no token found in authorization headers"};
+    if (!token) throw {type: 'unauthorized', message: "no token found in authorization headers"};
     const userData: any = manipulateToken.decryptToken(token);
     const findExistingUser = await usersRepository.findByEmail(userData.email);
     if (!findExistingUser) throw {type: 'NotFound', message: "user not found"};
