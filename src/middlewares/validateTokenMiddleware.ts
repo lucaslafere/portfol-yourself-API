724d323f9b19c7096174588e3a41c39b85d3fadf
Lucas Lafèré <laferelucas@gmail.com>
2022-10-05 18:14:33 -0300

    feat: better token error handling

--- src/middlewares/validateTokenMiddleware.ts
+++ src/middlewares/validateTokenMiddleware.ts
@@ -1,10 +1,18 @@
 import { Request, Response, NextFunction} from 'express';
 import * as usersRepository from '../repositories/usersRepository';
 import * as manipulateToken from '../utils/manipulateToken';
+import jwt from 'jsonwebtoken';
+
 
 export async function validateToken(req: Request, res: Response, next: NextFunction) {
+    const authorization = req.headers.authorization;
+    if (!authorization){
+        throw {type: 'not-found', message: "no token found in authorization headers"};
+    }
     const token = req.headers.authorization?.replace("Bearer ", "");
-    if (!token) throw {type: 'unauthorized', message: "no token found in authorization headers"};
+    jwt.verify(token, process.env.MY_SECRET_KEY, function (err, decoded) {
+        if (err) throw {type: 'unauthorized', message: "no token found in authorization headers"};
+    });
     const userData: any = manipulateToken.decryptToken(token);
     const findExistingUser = await usersRepository.findByEmail(userData.email);
     if (!findExistingUser) throw {type: 'NotFound', message: "user not found"};
