49a2f40adc52ab9d21cd2d8ff3ea12b703ef0d99
Lucas Lafèré <laferelucas@gmail.com>
2022-10-07 16:14:57 -0300

    fix: dotenv missing from server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import app from './app';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
