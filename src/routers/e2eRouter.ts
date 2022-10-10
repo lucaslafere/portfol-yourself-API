ba187b70cc025abfb83eef51d4f197f192de1f42
Lucas Lafèré <laferelucas@gmail.com>
2022-10-10 15:15:17 -0300

    feat: added e2e test route

--- src/routers/e2eRouter.ts
+++ src/routers/e2eRouter.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
-// import * as e2eController from '../controllers/e2eController';
+import * as e2eController from '../controllers/e2eController';
 
 const e2eRouter = Router();
-// e2eRouter.post('/e2e/reset', e2eController.reset);
+e2eRouter.post('/e2e/reset', e2eController.reset);
 
 export default e2eRouter
