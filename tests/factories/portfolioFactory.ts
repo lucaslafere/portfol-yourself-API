19fd6c72c5f520738834568e014a78af5fbb9411
Lucas Lafèré <laferelucas@gmail.com>
2022-10-10 16:56:40 -0300

    feat: added portfolios tests

--- tests/factories/portfolioFactory.ts
+++ tests/factories/portfolioFactory.ts
@@ -8,13 +8,16 @@ export default async function _portfolioFactory({
 }) {
     const portfolio = {
         title: faker.lorem.words(1),
+        userId: 1,
         logo: faker.internet.url() + ".jpg"
     };
     if (persist) {
         await prisma.portfolios.create({
             data: portfolio
         })
+        delete portfolio.userId
         return portfolio
     }
+    delete portfolio.userId
     return portfolio
 }
--- tests/int/portfolios.test.ts
+++ tests/int/portfolios.test.ts
@@ -21,7 +21,35 @@ beforeEach(async () => {
       expect(result.status).toBe(201);
       expect(createdPortfolio).not.toBeNull();
     })
+ 
+  it('Deve retornar 422 ao criar com body errado', async () => {
+    const user = await _userFactory({persist: true})
+      const login = await supertest(app).post('/sign-in').send(user)
+      const token = login.body.token;
+      const portfolio = await _portfolioFactory({});
+      delete portfolio.logo
+      const result = await supertest(app).post('/portfolios').set({Authorization: `Bearer ${token}`}).send(portfolio)
+
+      expect(result.status).toBe(422);
   })
+  it('Deve retornar 401 ao criar um portfolio sem token', async () => {
+    const user = await _userFactory({persist: true})
+    const login = await supertest(app).post('/sign-in').send(user)
+    const token = login.body.token;
+    const portfolio = await _portfolioFactory({});
+    const result = await supertest(app).post('/portfolios').send(portfolio)
+
+    expect(result.status).toBe(401);
+  })
+  it('Deve retornar 409 ao criar um portfolio que ja existe', async () => {
+    const user = await _userFactory({persist: true})
+    const login = await supertest(app).post('/sign-in').send(user)
+    const token = login.body.token;
+    const portfolio = await _portfolioFactory({persist: true});
+    const result = await supertest(app).post('/portfolios').set({Authorization: `Bearer ${token}`}).send(portfolio)
+    expect(result.status).toBe(409);
+  })
+});
   afterAll(async () => {
     await prisma.$disconnect();
   });
